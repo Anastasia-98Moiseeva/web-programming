4ce2ecea6c05039f83f72f70499ae2f925962227
Anastasia <nepro100nastya@yandex.ru>
2017-11-28 02:28:07 +0400

    change file

--- task4/main/solve.go
+++ task4/main/solve.go
@@ -3,39 +3,39 @@ package main
 import "unicode"
 import "strings"
 
-func RemoveEven(arr [] int) []int {
-	res := make([]int, 0)
-    for _, value := range (arr) {
-		if value % 2 == 1 {
-			result = append(res, value)
-		}
+func RemoveEven(x []int) []int {
+    res := make([]int, 0)
+    for _, val := range x {
+        if val % 2 == 1 {
+            res = append(res, val)
+        }
     }
     return res
 }
 
-func PowerGenerator(x int) (func() int) {
-	y := 1    
-	return func() (ret int) {        
-		y *= x       
-		return y   
-	}
+func PowerGenerator(x int) func() int {
+    y := 1
+    return func() int {
+        y = y*x
+        return y
+    }
 }
 
-func DifferentWordsCount(s string) int {
-	w := ""
-	res := 0
-	e := make(map[string]bool)
-	st := s + " ";
-	for _, let := range (st){
-		if unicode.IsLetter(let) {
-            w = append(w, string(unicode.ToUpper(let)))
-		} else if subst != "" {
-			if !e[let] {
-                res = append(res, 1)
-			}
-		e[w] = true
-		w = ""
-		}
-	}
-	return res
+func DifferentWordsCount(st string) int {
+    s := make(map[string]bool)
+    w := ""
+    res := 0
+    str = st + " "
+    for _, l := range (str) {
+        if unicode.IsLetter(l) {
+            w = append(w, string(unicode.ToLower(l)))
+        } else if w != "" {
+            if !s[w] {
+                res += 1
+            }
+            s[w] = true
+            w = ""
+        }
+    }
+    return res
 }
