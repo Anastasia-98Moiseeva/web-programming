37e6a324e6a765288b3fc30fc6bf07b944ee05bf
Anastasia <nepro100nastya@yandex.ru>
2017-11-28 01:59:09 +0400

    add files

---
+++ task4/main/solve.go
@@ -0,0 +1,40 @@
+package main
+
+import "unicode"
+
+func RemoveEven(arr [] int) []int {
+	res := []int{}
+    for _, value := range (arr) {
+		if value % 2 == 1 {
+			result = append(res, value)
+		}
+    }
+    return res
+}
+
+func PowerGenerator(x int) (func() int) {
+	y := 1    
+	return func() (ret int) {        
+		y *= x       
+		return y   
+	}
+}
+
+func DifferentWordsCount(s string) int {
+	w := ""
+	res := 0
+	e := make(map[string]bool)
+	st := s + " ";
+	for _, let := range (st){
+		if unicode.IsLetter(let) {
+            w = append(w, string(unicode.ToUpper(let)))
+		} else if subst != "" {
+			if !e[let] {
+                res += 1
+			}
+		e[w] = true
+		w = ""
+		}
+	}
+	return res
+}
